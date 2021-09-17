a729614791c8bdfb5e14e0a2b2a049ca1a89b2f5
AzureAD\GabrielNegreanu <gabriel.negreanu@principal33.com>
2021-09-17 17:10:59 +0300

    Added general style rules for TableHeader component

--- src/components/TableHeader.vue
+++ src/components/TableHeader.vue
@@ -12,4 +12,51 @@
 </script>
 
 <style scoped>
+.table-title {
+    padding-bottom: 15px;
+    color: #fff;
+    padding: 16px 30px;
+    margin: -20px -25px 10px;
+    border-radius: 3px 3px 0 0;
+    display: flex;
+    justify-content: space-between;
+    background: linear-gradient(90deg, #ff8348, #6235ff);
+    background-size: 100% 100%;
+    scroll-behavior: none;
+}
+
+.table-title h2 {
+    margin: 5px 0 0;
+    font-size: 24px;
+}
+
+.table-title .btn {
+    color: #566787;
+    float: right;
+    font-size: 13px;
+    background: #fff;
+    border: none;
+    min-width: 50px;
+    border-radius: 2px;
+    border: none;
+    outline: none !important;
+    margin-left: 10px;
+}
+
+.table-title .btn:hover,
+.table-title .btn:focus {
+    color: #566787;
+    background: #f2f2f2;
+}
+
+.table-title .btn i {
+    float: left;
+    font-size: 21px;
+    margin-right: 5px;
+}
+
+.table-title .btn span {
+    float: left;
+    margin-top: 2px;
+}
 </style>
